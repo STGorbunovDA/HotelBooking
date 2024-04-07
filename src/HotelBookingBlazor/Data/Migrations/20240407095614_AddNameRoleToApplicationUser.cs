92caf7f6cdd2c1ef441ddd4492e21c3e95cfe662
Дмитрий Горбунов <bous07@mail.ru>
2024-04-07 12:59:49 +0300

    Add Migration AddNameRoleToApplicationUser

---
+++ src/HotelBookingBlazor/Data/Migrations/20240407095614_AddNameRoleToApplicationUser.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelBookingBlazor.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNameRoleToApplicationUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContactNumber",
+                table: "AspNetUsers",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "varchar(10)",
+                unicode: false,
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "varchar(10)",
+                unicode: false,
+                maxLength: 10,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RoleName",
+                table: "AspNetUsers",
+                type: "varchar(8)",
+                unicode: false,
+                maxLength: 8,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContactNumber",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "RoleName",
+                table: "AspNetUsers");
+        }
+    }
+}
