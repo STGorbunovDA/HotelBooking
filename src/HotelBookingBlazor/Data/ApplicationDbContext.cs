363406bf55a0f539a98506535f2651eaea4c5e44
Дмитрий Горбунов <bous07@mail.ru>
2024-04-07 14:49:05 +0300

    Add ApplicationDbContext

--- src/HotelBookingBlazor/Data/ApplicationDbContext.cs
+++ src/HotelBookingBlazor/Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using HotelBookingBlazor.Data.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,5 +6,18 @@ namespace HotelBookingBlazor.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        public DbSet<Amenity> Amenities { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomType> RoomTypes { get; set; }
+        public DbSet<RoomTypeAmenity> RoomTypeAmenities { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+
+            builder.Entity<RoomTypeAmenity>()
+                .HasKey(ra => new { ra.RoomTypeId, ra.AmenityId });
+        }
     }
 }
