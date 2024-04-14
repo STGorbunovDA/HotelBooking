fcf437d8d8d697d673aabfafef55999b92b96ee1
Дмитрий Горбунов <bous07@mail.ru>
2024-04-14 08:30:58 +0300

    change RoomTypeAmenity

--- src/HotelBookingBlazor/Data/Entities/RoomTypeAmenity.cs
+++ src/HotelBookingBlazor/Data/Entities/RoomTypeAmenity.cs
@@ -3,8 +3,8 @@
     public class RoomTypeAmenity
     {
         public short RoomTypeId { get; set; }
-        public string AmenityId { get; set; }
-        public int Unit { get; set; }
+        public int AmenityId { get; set; }
+        public int? Unit { get; set; }
 
         public virtual RoomType RoomType { get; set; }
         public virtual Amenity Amenity { get; set; }
