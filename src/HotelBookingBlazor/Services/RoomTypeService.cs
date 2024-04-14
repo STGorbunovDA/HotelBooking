4e88c2dc48f22e8342a4065c2f94a7827a5917c4
Дмитрий Горбунов <bous07@mail.ru>
2024-04-14 08:30:01 +0300

    Add RoomTypeService

---
+++ src/HotelBookingBlazor/Services/RoomTypeService.cs
@@ -0,0 +1,61 @@
+﻿using HotelBookingBlazor.Data;
+using HotelBookingBlazor.Data.Entities;
+using HotelBookingBlazor.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBookingBlazor.Services
+{
+    public interface IRoomTypeService
+    {
+        Task<MethodResult<short>> CreateRoomTypeAsync(RoomTypeCreateModel model, string userId);
+    }
+
+    public class RoomTypeService : IRoomTypeService
+    {
+        private readonly IDbContextFactory<ApplicationDbContext> _contextFactory;
+
+        public RoomTypeService(IDbContextFactory<ApplicationDbContext> contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+
+        public async Task<MethodResult<short>> CreateRoomTypeAsync(RoomTypeCreateModel model, string userId)
+        {
+            using var context = _contextFactory.CreateDbContext();
+            if (await context.RoomTypes.AnyAsync(rt => rt.Name == model.Name))
+            {
+                return $"Номер с таким же названием {model.Name} уже существует";
+            }
+
+            var roomType = new RoomType
+            {
+                Name = model.Name,
+                AddedBy = userId,
+                AddedOn = DateTime.Now,
+                Description = model.Description,
+                Image = model.Image,
+                IsActive = model.IsActive,
+                MaxAdults = model.MaxAdults,
+                MaxChildren = model.MaxChildren,
+                Price = model.Price,
+            };
+
+            await context.RoomTypes.AddAsync(roomType);
+            await context.SaveChangesAsync();
+
+            if (model.Amenities.Length > 0)
+            {
+                var roomTypeAmentities = model.Amenities.Select(a => new RoomTypeAmenity
+                {
+                    AmenityId = a.Id,
+                    RoomTypeId = roomType.Id,
+                    Unit = a.Unit,
+                });
+                await context.RoomTypeAmenities.AddRangeAsync(roomTypeAmentities);
+                await context.SaveChangesAsync();
+            }
+
+            return roomType.Id;
+        }
+    }
+}
