67216c95bd8a4073ac48447820a3bf20e39266af
Дмитрий Горбунов <bous07@mail.ru>
2024-04-21 18:35:01 +0300

    Work with the class MethodResult

--- src/HotelBookingBlazor/Models/MethodResult.cs
+++ src/HotelBookingBlazor/Models/MethodResult.cs
@@ -8,4 +8,14 @@
         public static implicit operator MethodResult<TData>(TData data) => Success(data);
         public static implicit operator MethodResult<TData>(string errorMessage) => Failure(errorMessage);
     }
+
+    public readonly record struct MethodResult(bool IsSucces, string? ErrorMessage)
+    {
+        public static MethodResult Success() => new(true, null);
+        public static MethodResult Failure(string errorMessage) => new(false, errorMessage);
+
+        public static implicit operator MethodResult(bool isSucces) => new(isSucces, default);
+        public static implicit operator MethodResult(string errorMessage) => Failure(errorMessage);
+
+    }
 }
