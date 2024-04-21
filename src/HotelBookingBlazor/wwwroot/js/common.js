d1365a4401c62aac176df0c03427f57fb5a3e801
Дмитрий Горбунов <bous07@mail.ru>
2024-04-21 20:31:15 +0300

    add method alert in common.js

--- src/HotelBookingBlazor/wwwroot/js/common.js
+++ src/HotelBookingBlazor/wwwroot/js/common.js
@@ -1,4 +1,7 @@
 
 window.showModal = (dialogId) => document.getElementById(dialogId).showModal();
 
+//window.alertModal = (errorMessage) => document.getElementById().alert(errorMessage);
+window.alertModal = (errorMessage) => { window.alert(errorMessage);};
+
 window.closeModal = (dialogId) => document.getElementById(dialogId).close();
