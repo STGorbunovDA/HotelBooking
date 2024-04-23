19f220c388adf0cf515767f28c6d803e74b4c944
Дмитрий Горбунов <bous07@mail.ru>
2024-04-23 21:03:13 +0300

    Took out the class RegisterUserModel

---
+++ src/HotelBookingBlazor/Models/RegisterUserModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingBlazor.Models
+{
+    public class RegisterUserModel
+    {
+        [Required, MaxLength(15), RegularExpression(@"[a-zA-Z]+$")]
+        public string FirstName { get; set; } = "";
+
+        [MaxLength(15), RegularExpression(@"[a-zA-Z]+$")]
+        public string? LastName { get; set; } = "";
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; } = "";
+
+        [Required, MaxLength(15), RegularExpression(@"^((\+7|7|8)+([0-9]){10})$")]
+        public string ContactNumber { get; set; } = "";
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; } = "";
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; } = "";
+    }
+}
