0b6f5c849d5218c87ebc1b0a9064f3c77b68bc48
Дмитрий Горбунов <bous07@mail.ru>
2024-04-30 21:25:53 +0300

    Add EditStaffModel

---
+++ src/HotelBookingBlazor/Models/EditStaffModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingBlazor.Models
+{
+    public class EditStaffModel
+    {
+        public string Id { get; set; }
+
+        [Required, MaxLength(10), RegularExpression(@"[а-яА-Я]+$")]
+        public string FirstName { get; set; } = "";
+
+        [MaxLength(10), RegularExpression(@"[а-яА-Я]+$")]
+        public string? LastName { get; set; } = "";
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; } = "";
+
+        [Required, MaxLength(15), RegularExpression(@"^((\+7|7|8)+([0-9]){10})$")]
+        public string ContactNumber { get; set; } = "";
+
+        [MaxLength(50)]
+        public string? Designation { get; set; }
+    }
+}
