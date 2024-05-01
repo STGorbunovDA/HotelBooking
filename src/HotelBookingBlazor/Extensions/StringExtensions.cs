11ef2ca8a24df051cf4d96effd213a581acf7f77
Дмитрий Горбунов <bous07@mail.ru>
2024-05-01 19:19:34 +0300

    Added an extension method to return a string of strictly 100 characters

---
+++ src/HotelBookingBlazor/Extensions/StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelBookingBlazor.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Ellipsis(this string str, int maxLenght) =>
+            (string.IsNullOrWhiteSpace(str) || str.Length <= maxLenght)
+            ? str
+            : $"{str[0..97]}...";
+
+    }
+}
