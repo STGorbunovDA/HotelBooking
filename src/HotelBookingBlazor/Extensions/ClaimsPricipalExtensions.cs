77e39cd3abefca9f48fc71100dbb016c112d786c
Дмитрий Горбунов <bous07@mail.ru>
2024-05-01 17:59:02 +0300

    Add method Extensions - GetUserId in ClaimTypes.NameIdentifier

---
+++ src/HotelBookingBlazor/Extensions/ClaimsPricipalExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace HotelBookingBlazor.Extensions
+{
+    public static class ClaimsPricipalExtensions
+    {
+        public static string? GetUserId(this ClaimsPrincipal principal) =>
+            principal.FindFirstValue(ClaimTypes.NameIdentifier);
+    }
+}
