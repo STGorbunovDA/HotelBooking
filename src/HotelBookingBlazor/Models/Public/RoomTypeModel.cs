81801833b33bb7ac80d54aa5c86906643e811c8b
Дмитрий Горбунов <bous07@mail.ru>
2024-05-02 20:14:26 +0300

    Add RoomTypeModel

---
+++ src/HotelBookingBlazor/Models/Public/RoomTypeModel.cs
@@ -0,0 +1,7 @@
+﻿using HotelBookingBlazor.Data.Entities;
+
+namespace HotelBookingBlazor.Models.Public
+{
+    public readonly record struct RoomTypeAmenityModel(string Name, string? Icon = null, int? Unit = null);
+    public record RoomTypeModel(short Id, string Name, string Image, string Description, decimal Price, RoomTypeAmenityModel[] Amenities);
+}
