f2d28862efea7581ff82a5595689bf3356796fec
Дмитрий Горбунов <bous07@mail.ru>
2024-05-05 19:30:03 +0300

    Add class FilterModel

---
+++ src/HotelBookingBlazor/Services/Public/FilterModel.cs
@@ -0,0 +1,32 @@
+﻿namespace HotelBookingBlazor.Services.Public
+{
+    public class FilterModel
+    {
+        public FilterModel() { }
+
+        public static FilterModel GetFilterModel(DateOnly? checkInDate, DateOnly? checkOutDate, int? adults, int? children)
+        {
+            return new()
+            {
+                CheckInDate = checkInDate,
+                CheckOutDate = checkOutDate,
+                Adults = adults,
+                Children = children
+            };
+        }
+
+        public DateOnly? CheckInDate { get; set; } // = DateOnly.FromDateTime(DateTime.Today);
+        public DateOnly? CheckOutDate { get; set; } // = DateOnly.FromDateTime(DateTime.Today.AddDays(1));
+        public int? Adults { get; set; } = 0;
+        public int? Children { get; set; } = 0;
+
+        public IReadOnlyDictionary<string, object?> ToDictionary() =>
+            new Dictionary<string, object?>
+            {
+                [nameof(CheckInDate)] = CheckInDate,
+                [nameof(CheckOutDate)] = CheckOutDate,
+                [nameof(Adults)] = Adults,
+                [nameof(Children)] = Children,
+            };
+    }
+}
