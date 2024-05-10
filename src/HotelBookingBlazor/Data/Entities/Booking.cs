66d1b265bb3067bf446d9dc6833152da03c2003f
Дмитрий Горбунов <bous07@mail.ru>
2024-05-10 14:07:57 +0300

    Change main class Booking - Add :      - RoomType      - Status      - SpecialRequest      - InDate      - OutDate      - RoomId

--- src/HotelBookingBlazor/Data/Entities/Booking.cs
+++ src/HotelBookingBlazor/Data/Entities/Booking.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using HotelBookingBlazor.Constants;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 
 namespace HotelBookingBlazor.Data.Entities
@@ -8,23 +9,32 @@ namespace HotelBookingBlazor.Data.Entities
         [Key]
         public long Id { get; set; }
 
-        public int RoomId { get; set; }
+        public short RoomTypeId { get; set; }
+
+        public int? RoomId { get; set; }
 
         [Required]
         public string GuestId { get; set; }
         public int Adults { get; set; }
         public int Children {  get; set; }
-        public DateTime CheckInDate { get; set; }
-        public DateTime CheckOutDate { get; set; }
+        public DateOnly CheckInDate { get; set; }
+        public DateOnly CheckOutDate { get; set; }
 
         [Range(1, double.MaxValue)]
         public decimal TotalAmount { get; set; }
         public DateTime BookedOn { get; set; }
 
         [MaxLength(150), Unicode(false)]
+        public string? SpecialRequest { get; set; }
+
+        [MaxLength(150), Unicode(false)]
         public string? Remarks { get; set; }
+
+        public BookingStatus Status { get; set; } = BookingStatus.Pending;
        
         public virtual Room Room {  get; set; }
         public virtual ApplicationUser Guest { get; set; }
+
+        public virtual RoomType RoomType { get; set; }
     }
 }
