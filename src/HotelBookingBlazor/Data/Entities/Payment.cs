d4b043a58fd80f8ec488af24046027a007b6e927
Дмитрий Горбунов <bous07@mail.ru>
2024-05-12 20:06:38 +0300

    Add Payment

---
+++ src/HotelBookingBlazor/Data/Entities/Payment.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingBlazor.Data.Entities
+{
+    public class Payment
+    {
+        public Guid Id { get; set; } 
+        public long BookingId { get; set; }
+
+        [MaxLength(150)]
+        public string CheckoutSessionId { get; set; }
+
+        [MaxLength(10), Unicode(false)]
+        public string Status { get; set; }
+
+        [MaxLength(150), Unicode(false)]
+        public string? AdditionalInfo { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public virtual Booking Booking { get; set; }
+    }
+}
