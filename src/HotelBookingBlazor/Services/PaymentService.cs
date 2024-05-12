d1b0ccc0b04bb332e2a2b0a23384ac2c1448ab24
Дмитрий Горбунов <bous07@mail.ru>
2024-05-12 20:08:17 +0300

    Temp change PaymentService

--- src/HotelBookingBlazor/Services/PaymentService.cs
+++ src/HotelBookingBlazor/Services/PaymentService.cs
@@ -1,4 +1,9 @@
-﻿using HotelBookingBlazor.Models.Public;
+﻿using HotelBookingBlazor.Data;
+using HotelBookingBlazor.Data.Entities;
+using HotelBookingBlazor.Models;
+using HotelBookingBlazor.Models.Public;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Stripe.Checkout;
 
 namespace HotelBookingBlazor.Services
@@ -6,12 +11,38 @@ namespace HotelBookingBlazor.Services
     public interface IPaymentService
     {
         Task<string> GeneratePaymentUrl(PaymentModel model, string userId, string domain);
+        Task<MethodResult<string?>> ConfirmPaymentAsync(string paymentIdStr, long bookingId, string checkoutSessionId);
     }
 
     public class PaymentService : IPaymentService
     {
+        private readonly IDbContextFactory<ApplicationDbContext> _contextFactory;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private const string StripePaymentInitiated = "initiated ";
+        private const string StripePaymentSuccess = "paid";
+        private const string StripePaymentFail = "unpaid";
+        private const string StripePaymentPending = "Pending";
+        public PaymentService(IDbContextFactory<ApplicationDbContext> contextFactory,
+                              UserManager<ApplicationUser> userManager)
+        {
+            _contextFactory = contextFactory;
+            _userManager = userManager;
+        }
+
         public async Task<string> GeneratePaymentUrl(PaymentModel model, string userId, string domain)
         {
+            using var context = _contextFactory.CreateDbContext();
+
+            var paymentEntity = new Payment
+            {
+                BookingId = model.BookingId,
+                CreatedAt = DateTime.Now,
+                Status = StripePaymentInitiated,
+                CheckoutSessionId = StripePaymentPending
+            };
+            await context.Payments.AddAsync(paymentEntity);
+            await context.SaveChangesAsync();
+
             var totalAmount = model.NoOfDays * model.Price * 100;
 
             var lineItem = new SessionLineItemOptions
@@ -33,16 +64,76 @@ namespace HotelBookingBlazor.Services
             {
                 LineItems = [lineItem],
                 Mode = "payment",
-                SuccessUrl = domain + "/booking-success?session-id={CHECKOUT_SESSION_ID}",
-                CancelUrl = $"{domain}/booking-cancel"
+                SuccessUrl = $"{domain}/bookings/{model.BookingId}" + "/success?session-id={CHECKOUT_SESSION_ID}&payment-id=" + paymentEntity.Id,
+                CancelUrl = $"{domain}/bookings/{model.BookingId}/cancel&payment-id={paymentEntity.Id}"
             };
 
 
             var sessionService = new SessionService();
 
             var session = await sessionService.CreateAsync(sessionCreateOptions);
+
+            paymentEntity.CheckoutSessionId = session.Id;
+            await context.SaveChangesAsync();
+
             return session.Url;
 
+        }
+
+        public async Task<MethodResult<string?>> ConfirmPaymentAsync(string paymentIdStr, long bookingId, string checkoutSessionId)
+        {
+            using var context = _contextFactory.CreateDbContext();
+            var paymentEntity = await context.Payments
+                                             .AsTracking()
+                                             .FirstOrDefaultAsync(p => p.Id == Guid.Parse(paymentIdStr)
+                                                            && p.CheckoutSessionId == checkoutSessionId);
+
+            if (paymentEntity is null)
+            {
+                return new MethodResult<string?>(false, "Invalid payment id", default);
+            }
+
+            var booking = await context.Bookings
+                                           .AsTracking()
+                                           .FirstOrDefaultAsync(b => b.Id == bookingId);
+
+            if (booking is null)
+            {
+                return new MethodResult<string?>(false, "Invalid booking id", default);
+            }
+
+
+            if (paymentEntity.Status != StripePaymentInitiated)
+            {
+                // У нас уже есть этот статус оплаты
+                //  нам не нужно ничего делать с базой данных
+            }
+            else
+            {
+                var sessionService = new SessionService();
+                var checkoutSession = await sessionService.GetAsync(checkoutSessionId);
+                if (checkoutSession is null)
+                {
+                    return new MethodResult<string?>(false, "Invalid checkout session", default);
+                }
+
+
+                paymentEntity.Status = checkoutSession.PaymentStatus;
+                paymentEntity.AdditionalInfo = $"Name: {checkoutSession.CustomerDetails.Name} " +
+                                               $"Email: {checkoutSession.CustomerDetails.Email}";
+
+                
+                booking.Status = checkoutSession.PaymentStatus == StripePaymentSuccess
+                                    ? Constants.BookingStatus.PaymentSuccess
+                                    : Constants.BookingStatus.PaymentCancelled;
+
+                await context.SaveChangesAsync();
+            }
+            var guestName = await _userManager.Users
+                                              .Where(u => u.Id == booking.GuestId)
+                                              .Select(u => u.FullName)
+                                              .FirstOrDefaultAsync();
+            return new MethodResult<string?>(true, null, guestName);
         }
     }
 }
