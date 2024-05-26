8e7f2ebc927cc3e0ae945a482625d51c78c37351
Дмитрий Горбунов <bous07@mail.ru>
2024-05-26 19:50:56 +0300

    Add class js runtime extensions

---
+++ src/HotelBookingBlazor/Extensions/JsRuntimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+
+namespace HotelBookingBlazor.Extensions
+{
+    public static class JsRuntimeExtensions
+    {
+        public static async Task AlertAsync(this IJSRuntime jSRuntime, string message) =>
+            await jSRuntime.InvokeVoidAsync("window.alert", message);
+
+        public static async Task<bool> ConfirmAsync(this IJSRuntime jSRuntime, string message) =>
+            await jSRuntime.InvokeAsync<bool>("window.confirm", message);
+
+        public static async Task<string?> PromptAsync(this IJSRuntime jSRuntime, string message) =>
+            await jSRuntime.InvokeAsync<string?>("window.prompt", message);
+    }
+}
