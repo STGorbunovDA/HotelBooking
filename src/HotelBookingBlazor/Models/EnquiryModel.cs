4ccce36b1bd7116e6da2d70d1bd194533a6b05d7
Дмитрий Горбунов <bous07@mail.ru>
2024-05-26 19:49:04 +0300

    Add model Enquiry

---
+++ src/HotelBookingBlazor/Models/EnquiryModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingBlazor.Models
+{
+    public class EnquiryModel
+    {
+        [Required, MaxLength(20)]
+        public string Name { get; set; }
+
+        [Required, MaxLength(50), EmailAddress]
+        public string Email { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Subject { get; set; }
+
+        [Required, MaxLength(250)]
+        public string Message { get; set; }
+    }
+}
