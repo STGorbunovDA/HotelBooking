6f6836818692c9a46546c0b4cf4bd0030c14be0b
Дмитрий Горбунов <bous07@mail.ru>
2024-05-27 21:19:15 +0300

    Add Enquiry model

---
+++ src/HotelBookingBlazor/Data/Entities/Enquiry.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelBookingBlazor.Data.Entities
+{
+    public class Enquiry
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [Required, MaxLength(20), Unicode(false)]
+        public string Name { get; set; }
+
+        [Required, MaxLength(50), EmailAddress, Unicode(false)]
+        public string Email { get; set; }
+
+        [Required, MaxLength(50), Unicode(false)]
+        public string Subject { get; set; }
+
+        [Required, MaxLength(250), Unicode(false)]
+        public string Message { get; set; }
+
+        public DateTime EnquiredOn { get; set; }
+    }
+}
