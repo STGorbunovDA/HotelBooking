255949b37afd39f7978ade4005163c077d1e42ff
Дмитрий Горбунов <bous07@mail.ru>
2024-05-28 21:18:13 +0300

    Add Subscriber

---
+++ src/HotelBookingBlazor/Data/Entities/Subscriber.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingBlazor.Data.Entities
+{
+    public class Subscriber
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required, MaxLength(50), Unicode(false)]
+        public string Email { get; set;}
+
+        public DateTime SubscriberOn { get; set; }
+    }
+}
