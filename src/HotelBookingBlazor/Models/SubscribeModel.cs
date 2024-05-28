57e45cc52bcc0ad9634c601a4fb9d119169158a1
Дмитрий Горбунов <bous07@mail.ru>
2024-05-28 21:18:26 +0300

    Add SubscribeModel

---
+++ src/HotelBookingBlazor/Models/SubscribeModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingBlazor.Models
+{
+    public class SubscribeModel
+    {
+        [Required, MaxLength(50), EmailAddress, DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
